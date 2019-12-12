cc1bbb16e156e670850764f463a70e7f95de3825
github username <github email address>
2019-12-12 08:56:11 -0800

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,6 @@
 class Book
+  GENRES = ["Thriller", "Science Fiction", "Romance"]
+
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
@@ -9,5 +11,4 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
 end
