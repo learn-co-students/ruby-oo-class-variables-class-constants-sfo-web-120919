696095dfcfc9c74af70d83ebc824c45eff37451e
github username <github email address>
2019-12-14 16:32:06 -0800

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,10 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  #removed :genre from attr_accessor bc genre is being assigned
+
+  GENRES =[]
 
   def initialize(title)
     @title = title
@@ -10,4 +14,8 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
 end
